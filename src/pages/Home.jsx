ae5dad33e3b94174d273fb283ef3efea0a62f753
sandeep kamra <sandeep.d4d@gmail.com>
2025-03-03 18:58:25 +0530

    create login, register and forgot password, also made functionality for login and posts.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { allPosts } from "../data";
-import Post from '../components/Post';
+import Post from "../components/Post";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../redux/slices/authSlice";
+import { getAllPosts } from "../redux/slices/postSlice";
 
 const Home = () => {
-    const [favoritePosts, setFavoritePosts] = useState([]);
+  const [favoritePosts, setFavoritePosts] = useState([]);
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const {posts } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const savedFavorites =
+      JSON.parse(localStorage.getItem("favoritePosts")) || [];
+    setFavoritePosts(savedFavorites);
+  }, []);
 
-    useEffect(() => {
-        const savedFavorites = JSON.parse(localStorage.getItem("favoritePosts")) || [];
-        setFavoritePosts(savedFavorites);
-    }, []);
+  useEffect(()=>{
+    dispatch(getAllPosts());
+  },[dispatch])
 
-    const toggleFavorite = (postId) => {
-        let updatedFavorites;
-        if (favoritePosts.includes(postId)) {
-            updatedFavorites = favoritePosts.filter(id => id !== postId); // Remove from favorites
-        } else {
-            updatedFavorites = [...favoritePosts, postId]; // Add to favorites
-        }
+  const toggleFavorite = (postId) => {
+    let updatedFavorites;
+    if (favoritePosts.includes(postId)) {
+      updatedFavorites = favoritePosts.filter((id) => id !== postId); // Remove from favorites
+    } else {
+      updatedFavorites = [...favoritePosts, postId]; // Add to favorites
+    }
 
-        setFavoritePosts(updatedFavorites);
-        localStorage.setItem("favoritePosts", JSON.stringify(updatedFavorites)); // Save in storage
-    };
+    setFavoritePosts(updatedFavorites);
+    localStorage.setItem("favoritePosts", JSON.stringify(updatedFavorites)); // Save in storage
+  };
 
-    return (
-
-        <div className=" w-full max-w-4xl mx-auto">
-            {allPosts?.length > 0 &&
-                allPosts?.map((post) => {
-                    return (
-                        <Post
-                            key={post?.id}
-                            alignment="vertical"
-                            data={post}
-                            favoritePosts={favoritePosts}
-                            toggleFavorite={toggleFavorite}
-                        />
-                    );
-                })}
-        </div>
-    );
-}
+  console.log(posts, 'posts')
+  return (
+      <div className=" w-full max-w-4xl mx-auto">
+        {allPosts?.length > 0 &&
+          allPosts?.map((post) => {
+            return (
+              <Post
+                key={post?.id}
+                alignment="vertical"
+                data={post}
+                favoritePosts={favoritePosts}
+                toggleFavorite={toggleFavorite}
+              />
+            );
+          })}
+      </div>
+  );
+};
 
 export default Home;
